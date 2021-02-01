187869ea0167bcba0c69c86dccb04529d12a06d6
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-02-01 13:45:21 -0300

    Adjust camera resolution to 84x48

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    public float maxPixelHeight = 84f;
+    public float ScreenSizeOrthograpfic = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float scale = Mathf.Ceil(Screen.height / maxPixelHeight);
+        ScreenSizeOrthograpfic = Screen.height / 2f / scale;
+        Camera.main.orthographicSize = Screen.height / 2f / scale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
