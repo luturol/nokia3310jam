c7d24c9291ac0c7f9c6b556ccc487ffa7117c485
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-02-10 14:47:46 -0300

    Add camera following player around

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            Vector3 position = transform.position;
+            position.x = player.transform.position.x;
+            position.y = player.transform.position.y;
+
+            transform.position = position;
+        }
+    }
+}
