8f18506e5f4c4ba3b27578e8eed92b60e7d85216
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-02-10 14:43:32 -0300

    Add player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,11 @@ public class Player : MonoBehaviour
 {
     private PlayerInput playerInput;
     private Rigidbody2D rigid;
+
     [SerializeField] private float speed = 10f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     void Awake()
     {
         playerInput = new PlayerInput();
@@ -20,20 +24,35 @@ public class Player : MonoBehaviour
 
     void Movement(Vector2 moveInput)
     {
+        if (moveInput.x != 0 || moveInput.y != 0)
+        {
+
+            if (moveInput.x < 0)
+                spriteRenderer.flipX = true;
+            else
+                spriteRenderer.flipX = false;
+
+            animator.SetInteger("Walking", 1);
+        }
+        else
+        {
+            animator.SetInteger("Walking", 0);
+        }
+
         rigid.MovePosition(rigid.position + moveInput * speed * Time.fixedDeltaTime);
         rigid.velocity = moveInput * speed;
     }
-    
+
     void OnEnable()
     {
         playerInput.Enable();
     }
-   
+
     void OnDisable()
     {
         playerInput.Disable();
     }
-    
+
     void FixedUpdate()
     {
         Movement(playerInput.Movement.Move.ReadValue<Vector2>());
