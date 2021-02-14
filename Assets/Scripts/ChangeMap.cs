30ce8c6132c2fe6060643bd2feb85a8c8edaa37c
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-02-14 16:30:04 -0300

    Add scene loader and finish line

---
+++ Assets/Scripts/ChangeMap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMap : MonoBehaviour
+{
+    private SceneLoader sceneLoader;
+    
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();    
+    }
+    
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+            LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        sceneLoader.LoadNextScene();
+    }
+
+}
