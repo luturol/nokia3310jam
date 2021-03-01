964debd942c2f938c7854a327b536a665c11cdc4
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-03-01 10:53:26 -0300

    Add sound to menu buttons and add onclick functions to buttons

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,7 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-  public void PlayGame() {
+    [SerializeField] AudioClip clip;
+    [SerializeField] GameObject rankingMenu;
+    [SerializeField] GameObject optionsMenu;
+    [SerializeField] GameObject mainMenu;
+
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        this.audioSource = GetComponent<AudioSource>();
+    }
+
+    private void TriggerClickSound()
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void PlayGame()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-  }
+    }
+
+    public void EnableRankingMenu()
+    {
+        TriggerClickSound();
+        rankingMenu.SetActive(!rankingMenu.activeSelf);
+        mainMenu.SetActive(!mainMenu.activeSelf);
+    }
+
+    public void EnableOptionsMenu()
+    {
+        TriggerClickSound();
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+        mainMenu.SetActive(!mainMenu.activeSelf);
+    }
 }
