344a59e2436112691b3480dac2bb792180e5054d
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-03-07 20:29:53 -0300

    Add time in the map

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,19 +1,26 @@
-﻿using System.Collections;
+﻿using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using System;
 
 public class GameStatus : MonoBehaviour
 {
     [SerializeField] private float delayTimeToRestartGame = 5f;
     [SerializeField] private GameObject bustedImage;
     [SerializeField] private TextMeshProUGUI timeToRestartText;
+    [SerializeField] private TextMeshProUGUI timeText;    
 
     private bool restart = false;
     private float waitingTime = 0f;
     private Player player;
     private List<Monster> monsters;
+    private float timeInMap = 0f;
+    private int deaths = 0;
+    private DateTime date;
+    
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
@@ -48,6 +55,11 @@ public class GameStatus : MonoBehaviour
                     e.SetToInitialPosition();
                 });
             }
+        }
+        else{
+            timeInMap += Time.deltaTime;
+            date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0).AddSeconds(timeInMap);
+            timeText.text = date.ToString("mm:ss");
         }
     }
 
